73b89c97bcada79ab18e6ae7d62f30f50c64e630
Shahriar Fardows <shahriarfardows@gmail.com>
2024-07-27 16:49:02 +0600

    new server code

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require('http');
+const server = http.createServer(app);
+const socket = require('socket.io');
+const io = socket(server);
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+
+// Socket.io connection
+io.on('connection', (socket) => {
+
+
+    // user connection and disconnection messages
+    console.log('User connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+app.listen(port, () => console.log(`Server is running on  http://localhost:${port}/`));
